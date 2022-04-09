3f7f06bc23e47881337c6ae05905c6781978bf55
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-09 15:51:49 +0300

    added quote class

--- Quote/src/app/quote/quote.component.ts
+++ Quote/src/app/quote/quote.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[] = [
+    { quote: 'You must be the change you wish to see in the world.', sender:'April',Author:'Mahatma Ghandi',votes:0 },
+    {quote:'Genius is one percent inspiration and ninety-nine percent perspiration',sender:'Negan',Author:'Thomas Edison',votes:0 },
+    {quote: 'If at first you don’t succeed, try, try again.',sender:'Sara',Author:'W.E Hickson',votes:0 },
+  ];
   constructor() { }
 
   ngOnInit(): void {
