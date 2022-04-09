2402241439a5a9c1d4ba4bdfc6ae11cccb3d45f3
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-09 16:06:24 +0300

    inputed an upvote and downvote button

--- Quote/src/app/quote/quote.component.ts
+++ Quote/src/app/quote/quote.component.ts
@@ -11,6 +11,13 @@ export class QuoteComponent implements OnInit {
     {quote:'Genius is one percent inspiration and ninety-nine percent perspiration',sender:'Negan',Author:'Thomas Edison',votes:0 },
     {quote: 'If at first you don’t succeed, try, try again.',sender:'Sara',Author:'W.E Hickson',votes:0 },
   ];
+   votescount: number = 0;
+  functionlike() {
+    this.votescount++;
+  }
+  functiondislike() {
+    this.votescount--;
+  }
   constructor() { }
 
   ngOnInit(): void {
