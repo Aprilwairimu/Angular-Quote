d53f0d4ea32f96a1e5d3da76b98a5219319c5dd7
Aprilwairimu <april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.comgit config --global user.email april.wairimu@student.moringaschool.com>
2022-04-11 13:17:58 +0300

    modified the postdate

--- Quote/src/app/timepipe.pipe.ts
+++ Quote/src/app/timepipe.pipe.ts
@@ -7,13 +7,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimepipePipe implements PipeTransform {
     transform(value: any): any {
     let Today: Date = new Date();
-    let dateToday: any = new Date(
-      Today.getFullYear(),
-      Today.getMonth(),
-      Today.getDay()
-    );    var timelapse = Math.abs(dateToday - value);
-    const secs = 86400;
-    var difference = timelapse / secs;    return difference;
+    // let dateToday: any = new Date(
+    //   Today.getFullYear(),
+    //   Today.getMonth(),
+    //   Today.getDay()
+    //   );
+    //   var timelapse = Math.abs(dateToday - value);
+    // const secs = 86400;
+      // var difference = timelapse / secs;    
+      return Today;
   }
 }
 
